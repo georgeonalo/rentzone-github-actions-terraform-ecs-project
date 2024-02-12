5a72813c34669fa1b68ec3f621ea4fad5fefd817
george onalo <onalotech7@gmail.com>
2024-02-12 20:29:00 +0100

    apply

--- iac/backend.tf
+++ iac/backend.tf
@@ -5,6 +5,6 @@ terraform {
     key            = "rentzone-app/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-lock"
-    profile = "terraform-user"
+   # profile = "terraform-user"
   }
 }
